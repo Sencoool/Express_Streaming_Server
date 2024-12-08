080949af990f4c5c5494ae0eb2e0e01352e98549
Sencoool <s6506021610037@email.kmutnb.ac.th>
2024-12-08 17:01:34 +0700

    Recreate

--- backendcrud.js
+++ backendcrud.js
@@ -110,51 +110,9 @@ const User = sequelize.define("user", {
     defaultValue: "User",
     allowNull: false,
   },
-  profilePicture: {
-    type: Sequelize.STRING,
-    defaultValue: "noimage.jpg",
-    allowNull: true,
-  },
 });
 
-const Favorite = sequelize.define("favorite", {
-  fmovie_id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  movie_id: {
-    type: Sequelize.INTEGER,
-  },
-  user_id: {
-    type: Sequelize.INTEGER,
-  },
-});
-
-const Review = sequelize.define("review", {
-  review_id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  movie_id: {
-    type: Sequelize.INTEGER,
-  },
-  user_id: {
-    type: Sequelize.INTEGER,
-    defaultValue: 0,
-  },
-  score: {
-    type: Sequelize.INTEGER,
-    allowNull: true,
-  },
-  comment: {
-    type: Sequelize.STRING,
-    allowNull: true,
-  },
-});
-
-sequelize.sync(); //if table not exist create
+sequelize.sync({}); //if table not exist create
 
 app.get("/movies", (req, res) => {
   Movies.findAll() //select * from
@@ -189,32 +147,15 @@ app.get("/moviedelete/", (req, res) => {
 app.get("/movie/:id", (req, res) => {
   Movies.findByPk(req.params.id)
     .then((movie) => {
-      Review.findAll({ where: { movie_id: req.params.id } }).then((review) => {
-        let ar = [review, movie];
-        // console.log(review, movie, users);
-        console.log(movie);
+      let ar = [movie];
+      // console.log(review, movie, users);
+      console.log(movie);
 
-        res.json(ar);
-      });
+      res.json(ar);
     })
     .catch((err) => {
       res.status(500).send(err);
     });
-});
-
-app.post("/movie/:id", async (req, res) => {
-  try {
-    const checkingFavorite = await Favorite.findOne({
-      where: {
-        movie_id: req.params.id,
-        user_id: req.body.user_id,
-      },
-    });
-    if (checkingFavorite) res.json({ message: true });
-    else res.json({ message: false });
-  } catch (err) {
-    res.status(500).send(err);
-  }
 });
 
 app.post("/movies", upload.single("imageFile"), (req, res) => {
@@ -246,14 +187,15 @@ app.put("/movie/:id", upload.single("imageFile"), (req, res) => {
       if (!movie) {
         res.status(404).send("Movie not found");
       } else {
-        if (req.file.filename != undefined) {
+        if (req.file && req.file.filename) {
           console.log(req.file.filename);
           req.body.imageFile = req.file.filename;
+
+          // ลบไฟล์เก่าออกจากระบบ (เฉพาะเมื่อมีการอัพโหลดไฟล์ใหม่)
           const imagePath = path.join(
             __dirname,
             `/imageFile/${movie.imageFile}`
           );
-
           fs.unlink(imagePath, (err) => {
             if (err) {
               console.log("Error deleting file:", err);
@@ -261,13 +203,15 @@ app.put("/movie/:id", upload.single("imageFile"), (req, res) => {
               console.log("File deleted successfully");
             }
           });
+        } else {
+          req.body.imageFile = movie.imageFile;
         }
+
+        // อัปเดตข้อมูลของหนังในฐานข้อมูล
         movie
           .update(req.body)
           .then(() => {
-            // console.log(req.body);
             console.log(req);
-
             res.send(movie);
           })
           .catch((err) => {
@@ -281,16 +225,6 @@ app.put("/movie/:id", upload.single("imageFile"), (req, res) => {
 });
 
 app.delete("/movie/:id", (req, res) => {
-  Favorite.destroy({
-    where: {
-      movie_id: req.params.id,
-    },
-  });
-  Review.destroy({
-    where: {
-      movie_id: req.params.id,
-    },
-  });
   Movies.findByPk(req.params.id)
     .then((movie) => {
       if (!movie) {
@@ -358,18 +292,10 @@ app.post("/login", upload.single(""), async (req, res) => {
     return res.status(200).json({ message: true, token });
   } catch (error) {
     console.error(error);
+    console.log("hello");
+
     return res.status(500).json({ error: "Server_error" });
   }
-});
-
-app.get("/users", (req, res) => {
-  User.findAll() //select * from
-    .then((users) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
 });
 
 app.get("/user/:id", (req, res) => {
@@ -386,57 +312,12 @@ app.get("/user/:id", (req, res) => {
     });
 });
 
-app.put("/admin/:id", (req, res) => {
-  User.findByPk(req.params.id)
-    .then((user) => {
-      if (!user) res.status(404).send("user not found");
-      if (user.roles == "Admin") {
-        user
-          .update({ roles: "User" })
-          .then(() => {
-            res.send(user);
-          })
-          .catch((err) => {
-            res.status(500).send(err);
-          });
-      } else {
-        user
-          .update({ roles: "Admin" })
-          .then(() => {
-            res.send(user);
-          })
-          .catch((err) => {
-            res.status(500).send(err);
-          });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
-});
-
 app.put("/user/:id", (req, res) => {
   User.findByPk(req.params.id)
     .then((user) => {
       if (!user) {
         res.status(404).send("user not found");
       } else {
-        if (
-          req.body.profilePicture != undefined &&
-          user.profilePicture != "noimage.jpg"
-        ) {
-          const imagePath = path.join(
-            __dirname,
-            `/public/images/${user.profilePicture}`
-          );
-          fs.unlink(imagePath, (err) => {
-            if (err) {
-              console.log("Error deleting file:", err);
-            } else {
-              console.log("File deleted successfully");
-            }
-          });
-        }
         user
           .update(req.body)
           .then(() => {
@@ -451,178 +332,6 @@ app.put("/user/:id", (req, res) => {
     .catch((err) => {
       res.status(500).send(err);
     });
-});
-
-app.delete("/user/:id", (req, res) => {
-  User.findByPk(req.params.id)
-    .then((user) => {
-      if (!user) {
-        res.status(404).send("User not found");
-      } else {
-        if (user.profilePicture != "noimage.jpg") {
-          const imagePath = path.join(
-            __dirname,
-            `/public/images/${user.profilePicture}`
-          );
-          fs.unlink(imagePath, (err) => {
-            if (err) {
-              console.log("Error deleting file:", err);
-            } else {
-              console.log("File deleted successfully");
-            }
-          });
-        }
-      }
-      user
-        .destroy()
-        .then(() => {
-          res.send({ message: "Delete Successfully" });
-        })
-        .catch((err) => {
-          res.status(500).send(err);
-        });
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
-});
-
-app.get("/favorite/:id", (req, res) => {
-  Favorite.findAll({ where: { user_id: req.params.id } })
-    .then((e) => {
-      Movies.findAll().then((f) => {
-        let moviearr = [];
-        for (let i = 0; i < e.length; i++)
-          for (let j = 0; j < f.length; j++)
-            if (e[i].dataValues.movie_id == f[j].dataValues.movie_id)
-              moviearr.push(f[j]);
-
-        res.json(moviearr);
-      });
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
-});
-
-app.post("/favorite", async (req, res) => {
-  try {
-    const existingFavorite = await Favorite.findOne({
-      where: {
-        movie_id: req.body.movie_id,
-        user_id: req.body.user_id,
-      },
-    });
-
-    if (existingFavorite) return res.json({ message: "al" });
-
-    const favorite = await Favorite.create({
-      movie_id: req.body.movie_id,
-      user_id: req.body.user_id,
-    });
-
-    res.send(favorite);
-  } catch (error) {
-    console.error("Error creating favorite:", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-app.delete("/favorite", async (req, res) => {
-  try {
-    const existingFavorite = await Favorite.findOne({
-      where: {
-        movie_id: req.body.movie_id,
-        user_id: req.body.user_id,
-      },
-    });
-
-    if (!existingFavorite)
-      return res.send.json({ message: "Favorite not found" });
-
-    await Favorite.destroy({
-      where: {
-        movie_id: req.body.movie_id,
-        user_id: req.body.user_id,
-      },
-    });
-
-    res.json({ message: "Favorite successfully deleted" });
-  } catch (error) {
-    console.error("Error deleting favorite:", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-app.get("/typemovie", (req, res) => {
-  Movies.findAll({
-    where: {
-      type: "Movie",
-    },
-  }) //select * from
-    .then((movies) => {
-      res.json(movies);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
-});
-
-app.get("/typeseries", (req, res) => {
-  Movies.findAll({
-    where: {
-      type: "Series",
-    },
-  }) //select * from
-    .then((movies) => {
-      res.json(movies);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
-});
-
-app.get("/review/:id", (req, res) => {
-  Review.findAll({ where: { review_id: req.params.id } }) // find where review id = id on URL
-    .then((reviews) => {
-      res.json(reviews);
-      // console.log(reviews);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
-});
-
-app.post("/review/:id", async (req, res) => {
-  try {
-    const review = await Review.create({
-      movie_id: req.body.movie_id,
-      user_id: req.body.user_id,
-      score: req.body.score,
-      comment: req.body.comment,
-    });
-
-    res.send(review);
-  } catch (error) {
-    console.error("Error creating favorite:", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-// route to delete a review
-app.delete("/review/:id", async (req, res) => {
-  try {
-    await Review.destroy({
-      where: {
-        review_id: req.params.id,
-      },
-    });
-
-    res.json({ message: "Review successfully deleted" });
-  } catch (error) {
-    console.error("Error deleting favorite:", error);
-    res.status(500).send("Internal Server Error");
-  }
 });
 
 const port = process.env.PORT || 3000;
